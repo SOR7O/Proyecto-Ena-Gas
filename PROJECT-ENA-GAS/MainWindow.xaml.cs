4dc8fbef752244fe89bd374335ff23e78bdbdb03
AngelSorto <AngelSorto@DESKTOP-H26444U>
2019-07-27 12:41:39 -0700

    pantallas

--- PROJECT-ENA-GAS/MainWindow.xaml.cs
+++ PROJECT-ENA-GAS/MainWindow.xaml.cs
@@ -20,48 +20,9 @@ namespace PROJECT_ENA_GAS
     /// </summary>
     public partial class MainWindow : Window
     {
-        BaseDataContext dataContext;
         public MainWindow()
         {
             InitializeComponent();
-            dataContext = new BaseDataContext();
-            dt.ItemsSource = dataContext.Inventario;
-            llenarcmbInventario();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Inventario iv = new Inventario()
-            {
-                idCantidad = Convert.ToInt32(txtid.Text),
-                cantidad = Convert.ToInt32(txtCantidad.Text)
-            };
-            var existe = (from tv in dataContext.Inventario
-                          where tv.idCantidad == iv.idCantidad
-                          select tv).SingleOrDefault();
-            if (existe==null)
-            {
-                dataContext.Inventario.InsertOnSubmit(iv);
-                dataContext.SubmitChanges();
-            }
-            else
-            {
-                existe.cantidad = iv.cantidad;
-                dataContext.SubmitChanges();
-            }
-            dt.ItemsSource = dataContext.Inventario;
-        }
-        public void llenarcmbInventario()
-        {
-            BaseDataContext dataContext = new BaseDataContext();
-
-            var trarDatos = (from q in dataContext.Inventario
-                             select new { q.idCantidad}).ToList();
-            cmbLista.ItemsSource = trarDatos;
-            cmbLista.DisplayMemberPath = "idCantidad";
-            cmbLista.SelectedValue = "idCantidad";
-            
-
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -82,4 +43,3 @@ namespace PROJECT_ENA_GAS
         }
     }
 }
-
--- PROJECT-ENA-GAS/Usuario.xaml.cs
+++ PROJECT-ENA-GAS/Usuario.xaml.cs
@@ -10,9 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 
 namespace PROJECT_ENA_GAS
 {
@@ -21,57 +19,9 @@ namespace PROJECT_ENA_GAS
     /// </summary>
     public partial class Ausuario : Window
     {
-        BaseDataContext dataContext;
         public Ausuario()
         {
             InitializeComponent();
-            dataContext = new BaseDataContext();
-            dtU.ItemsSource = dataContext.Usuario;
-            LlenarCmb();
-        }
-
-
-        private void BtnA_Click(object sender, RoutedEventArgs e)
-        {
-            Usuario us = new Usuario()
-            {
-                nombreUsuario = txtNombre.Text,
-                contraseña = txtContra.Password,
-                cargo = cmbDatos.SelectedValue.ToString()
-            };
-            var existe = (from tu in dataContext.Usuario
-                          where tu.idUsuario == us.idUsuario
-                          select tu).SingleOrDefault();
-
-            if (existe == null)
-            {
-                dataContext.Usuario.InsertOnSubmit(us);
-                dataContext.SubmitChanges();
-            }
-            else
-            {
-                existe.nombreUsuario = us.nombreUsuario;
-                existe.contraseña = us.contraseña;
-                existe.cargo = us.cargo;
-                dataContext.SubmitChanges();
-            }
-            dtU.ItemsSource = dataContext.Usuario;
-
-        }
-        private void LlenarCmb()
-        {
-            BaseDataContext dataContext = new BaseDataContext();
-
-            var trarDatos = (from q in dataContext.Cargo
-                             select new { q.cargoUsu} ).ToList();
-            cmbDatos.ItemsSource = trarDatos;
-           cmbDatos.DisplayMemberPath = "cargoUsu";
-            cmbDatos.SelectedValue = "idCargo";
-        }
-
-        private void BtnRegresarMenu_Click(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
         }
 
         private void BtnRegresar_Click(object sender, RoutedEventArgs e)
