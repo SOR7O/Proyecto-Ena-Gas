e725df91b280d1300b8f4974804440a1ac9c95e1
AngelSorto <AngelSorto@DESKTOP-H26444U>
2019-08-04 21:32:31 -0700

    Update ENAGAS.sql
    Base de datos final

--- SQL/ENAGAS.sql
+++ SQL/ENAGAS.sql
@@ -1,18 +1,22 @@
 USE tempdb
 GO
 
+--Crear las base de datos
 IF NOT EXISTS(SELECT * FROM sys.databases WHERE [name] = 'ENAGAS')
 	BEGIN
 		CREATE DATABASE ENAGAS
 	END
 GO
 
+--Usar la base de datos
 USE ENAGAS
 GO
 
+--Creacion de Esquema 
 CREATE SCHEMA EnaGas
 GO
 
+--Tabla donde registran los clientes
 CREATE TABLE EnaGas.ClientesEna(
 idCliente INT IDENTITY(1,1) PRIMARY KEY CLUSTERED NOT NULL,
 identidad VARCHAR(13)NOT NULL,
@@ -25,14 +29,14 @@ cantidad INT
 )
 GO
 
-
-
+--Tabla que registra el total de chimbos sin importar el peso, en general
 CREATE TABLE EnaGas.Inventario(
 idCantidad INT IDENTITY(1,1)NOT NULL,
 cantidad INT NOT NULL PRIMARY KEY CLUSTERED
 )
 GO
 
+--Tabla donde se registran los chimbos por peso
 CREATE TABLE EnaGas.Chimbo(
 idChimbo INT IDENTITY(1,1)NOT NULL,
 cantidad int NOT NULL,
@@ -41,23 +45,34 @@ precio MONEY NOT NULL
 )
 GO
 
+--Tabla donde se registran los usuarios
 CREATE TABLE EnaGas.Usuario(
 idUsuario INT IDENTITY(1,100)NOT NULL PRIMARY KEY CLUSTERED,
 nombreUsuario NVARCHAR(100)NOT NULL,
 contraseña NVARCHAR(100)NOT NULL,
+idCargo INT,
 cargo NVARCHAR(50)NOT NULL
 )
 GO
 
+--Tabla donde estan registrados los tipos de cargos del usuario
 CREATE TABLE EnaGas.Cargo(
-idCargo INT IDENTITY NOT NULL,
+idCargo INT IDENTITY NOT NULL PRIMARY KEY CLUSTERED,
 cargoUsuario NVARCHAR(50) NOT NULL
 )
 GO
 
-ALTER TABLE EnaGas.Chimbo
-ADD CONSTRAINT FK_EnaGas_Chimbo_Id$TieneUna$EnaGas_Inventario
-FOREIGN KEY (idCantidad) REFERENCES EnaGas.Inventario(idCantidad)
+--Tabla de registro del total por venta
+CREATE TABLE EnaGas.TotalVenta(
+idTotal INT IDENTITY NOT NULL PRIMARY KEY CLUSTERED,
+totalVenta MONEY,
+fecha DATETIME DEFAULT GETDATE())
+GO
+
+--Llave foranea de el usuario
+ALTER TABLE EnaGas.Usuario
+ADD CONSTRAINT FK_EnaGas_Usuario_idCargo$TieneUn$EnaGas_Cargo_idCargo
+FOREIGN KEY(idCargo) REFERENCES EnaGas.Cargo(idCargo)
 ON UPDATE CASCADE
 ON DELETE NO ACTION
 GO
@@ -65,6 +80,8 @@ GO
 
 
 
+
+--Procedimiento almacenado para agregar n cantidad de chimbos y actualizar el inventario
 CREATE PROCEDURE EnaGas.AGREGAR_CHIMBO @cantidad INT,@precio MONEY,@peso NVARCHAR(20)
 AS
   BEGIN TRANSACTION
@@ -89,6 +106,9 @@ AS
 		   END CATCH
 GO
 
+--Procedimiento almacenado para agregar una vente junto con los datos del cliente
+--Actualiza el inventario 
+--Actualiza la cantidad de chimbos que salen por peso
 CREATE PROCEDURE EnaGas.AGREGAR_VENTA 
 @identidad VARCHAR(13),
 @nombre NVARCHAR(50),
@@ -132,4 +152,15 @@ AS
 	  ROLLBACK TRANSACTION
 	END CATCH
 GO
+
+
+--Procedimiento almacenado para sacar el total de la venta
+CREATE PROCEDURE EnaGas.TOTAL_VENTA @cantidad INT ,@peso NVARCHAR(100)
+AS
+INSERT INTO EnaGas.TotalVenta(totalVenta)
+SELECT precio * @cantidad FROM EnaGas.Chimbo 
+WHERE peso=@peso
+GO
+
+
 
